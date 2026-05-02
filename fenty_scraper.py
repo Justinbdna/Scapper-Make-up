cdbd8a93591247267c8f68b8e29a5f7139aacc98
Justinbnda <bandiola.justin@yahoo.com>
2026-05-02 17:32:18 +0200

    feat: Boucle d'extraction des titres et URLs via le JSON Shopify

--- fenty_scraper.py
+++ fenty_scraper.py
@@ -1,9 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://fentybeauty.com/en-fr/collections/makeup-face-foundation"
+URL = "https://fentybeauty.com/en-fr/collections/makeup-face-foundation.json"
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
 response = requests.get(URL, headers=headers)
+data = response.json()
 
-soup = BeautifulSoup(response.text, "html.parser")
-print("Statut HTTP:", response.status_code)
+print("Nombre de produits trouvés :", len(data["products"]))
+
+for product in data["products"][:5]:
+    titre = product["title"]
+    url_produit = f"https://fentybeauty.com/en-fr/products/{product['handle']}"
+    print(f"{titre} | {url_produit}")
