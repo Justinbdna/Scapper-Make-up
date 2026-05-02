ab9b6f0ee1ef415e807a583600d916cf1a31ea54
Justinbnda <bandiola.justin@yahoo.com>
2026-05-02 17:39:52 +0200

    feat: Export des données scrapées vers un fichier CSV

--- fenty_scraper.py
+++ fenty_scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 URL = "https://fentybeauty.com/en-fr/collections/makeup-face-foundation/products.json"
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
@@ -8,7 +9,8 @@ data = response.json()
 
 print("Nombre de produits trouvés :", len(data["products"]))
 
-for product in data["products"][:5]:
-    titre = product["title"]
-    url_produit = f"https://fentybeauty.com/en-fr/products/{product['handle']}"
-    print(f"{titre} | {url_produit}")
+with open("fenty_teint.csv", "w", newline="", encoding="utf-8") as fichier:
+    writer = csv.writer(fichier)
+    writer.writerow(["Titre", "URL"])
+    for product in data["products"]:
+        writer.writerow([product["title"], f"https://fentybeauty.com/en-fr/products/{product['handle']}"])
