f8d4904a4ed04406b9bf859d9c455fa288b1d6a1
Justinbnda <bandiola.justin@yahoo.com>
2026-05-02 17:30:08 +0200

    feat: Ajout du parsing BeautifulSoup et test de connexion

--- fenty_scraper.py
+++ fenty_scraper.py
@@ -4,3 +4,6 @@ from bs4 import BeautifulSoup
 URL = "https://fentybeauty.com/en-fr/collections/makeup-face-foundation"
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
 response = requests.get(URL, headers=headers)
+
+soup = BeautifulSoup(response.text, "html.parser")
+print("Statut HTTP:", response.status_code)
